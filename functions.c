0114b488a7572c958f0ad3f11685b5efd5f56601
Adam Kessler <dsplayer14cp@gmail.com>
2013-08-22 01:35:13 -0400

    2AM great idea

--- functions.c
+++ functions.c
@@ -2,4 +2,8 @@
 #include <netinet/in.h>
 
 /* Get socket for specific location, protocl, and port */
-int getsocket(package package, char const * const url);
+int getsocket(package *pack) {
+	char url_parse[(sizeof(char)*(*pack->url)) + 1];
+	strcpy(url_parse, *pack->url);
+	while()
+}
