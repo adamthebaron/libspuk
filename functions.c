b08a2f613ab30252a7ac8092e48faea2b28266a5
Adam Kessler <dsplayer14cp@gmail.com>
2013-08-24 09:39:35 -0400

    school allowed.

--- functions.c
+++ functions.c
@@ -1,9 +1,20 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <spuk/typedefs.c>
+#include <stdlib.h>
 
-/* Get socket for specific location, protocl, and port */
+/* Get socket for specific location, protocol, and port */
 int getsocket(package *pack) {
-	char url_parse[(sizeof(char)*(*pack->url)) + 1];
-	strcpy(url_parse, *pack->url);
-	while()
+char url_parse[(sizeof(char)*(*pack->url)) + 1];
+unsigned i = 0; //Increment variable
+while(url_parse != ':') {
+url_parse[i] = *pack->url[i];
+i++;
+}
+i = 0;
+char protocol[5];
+protocol = url_parse;
+if (url_parse[i] != '\0')
+url_parse[i] = '\0';
+
 }
