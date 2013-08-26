b91a694350a84d68454b828c0e4e3a8bc6140790
Adam Kessler <dsplayer14cp@gmail.com>
2013-08-25 23:11:29 -0400

    icantc

--- spukfuncs.c
+++ spukfuncs.c
@@ -10,11 +10,17 @@
    variables of a spuket */
 
 int getspuket(package *pack) {
-	char url_protocol[sizeof(char) * 5];
-	char top_level_domain[sizeof(char) * 128];
+	char url_protocol[5];          //ftp://
+	char top_level_domain[128];    //example.com/
+	char path_to_file[128];		   //folder/directory/files/
+	char filename[64];			   //program.tar.gz
 	memcpy(url_protocol, (const char *) &pack->url, sizeof(char) * 4);
 	if (url_protocol[5] == ':')
 		url_protocol[5] == '\0';
 	memcpy(top_level_domain, (const char *) &pack->url, sizeof(char) * 6);
-
+	int i;
+	for(i = 0; i <= (sizeof(top_level_domain)); i++) {
+		if (top_level_domain[i] == ':' || top_level_domain[i] == '/')
+			top_level_domain[i] = '\0';
+	}
 }
