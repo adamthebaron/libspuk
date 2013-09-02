37a1ba1e5d123236fc027fc536afc31d04bf1198
Adam Paul Kessler <dsplayer14cp@gmail.com>
2013-09-02 00:29:47 +0000

    I suck at this

--- spukdefs.c
+++ spukdefs.c
@@ -1,22 +1,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-
-
-/* Package structure which is used with a socket */
+/* Package structure which is used with a socket and has a variety of different flags, usually used for spuk the package manager */
 typedef struct{
-	/* URL to EXACT location where tarball is. */
-	char const * const url;
-	/* Y-you know what... I don't know why I declared this. */
-	//char const * const filename;
-	/* Location of tarball on server (Parent directory) */
-	char const * const location;
-	/* Size of tarball in bytes */
-	float size;
-	/* Checksums to be safe */
-	char const * const MD5SUM;
-	char const * const SHA512SUM;
+  /* Only download, do not install */
+  bool no_install;
+  /* Edit configure script if present */
+  bool edit_configure;
+  /* Edit makefile script if present */
+  bool edit_makefile; 
+  /* URL to EXACT location where tarball is. */
+  char const * const url;
+  /* Y-you know what... I don't know why I declared this. */
+  //char const * const filename;
+  /* Location of tarball on server (Parent directory) */
+  char const * const location;
+  /* Size of tarball in bytes */
+  float size;
+  /* Checksums to be safe */
+  char const * const MD5SUM;
+  char const * const SHA512SUM;
 }package;
 
 
@@ -25,20 +30,19 @@ typedef struct{
 typedef struct{
 	/* Package that we will be dealing with */
 	package * software;
-	/* Array of file paths that point to EACH file that the package has installed */
- 	char (* files[])[];
 	/* Number of files added */
 	int files_added;
 	/* Number of files removed */
 	int files_removed;
+        /* Array of file paths that point to EACH file that the package has installed */
+ 	char const * const files[];
 }profile;
 
 
 
 /* wrapper around the standard socket which helps with the creation of a socket tailored to a package */
 typedef struct{
-        /* Address to connect() to. */
-        char const * const address;
+
 	/* Protocol family to use. For example, AF_INET, which is a bunch of protocols to use with IPv4 */
 	const int address_family;
 	/* The type of transmission between the two computers. For example, SOCK_STREAM for a reliable connection */
@@ -50,4 +54,5 @@ typedef struct{
 	/* And finally, the regular socket. This is a function pointer to a socket() call with it's parameters
 	  equal to the values in address_family, socket_type, and protocol*/
 	int (*sock_desc) (int address_family, int socket_type, int protocol);
+	sock_desc = socket;
 }spuket;
